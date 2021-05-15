f6a6d0cb9cfabecf5b13e1d55a6366b340e9c40d
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-05-16 06:44:33 +0700

    Minor change to handle bodyParser deprecation

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Connect to MongoDB
